de5fdd12462d26787e85638d123766e67389fd2e
Giuseppe Amato <amato.ct@gmail.com>
2022-09-23 01:12:09 +0200

    Implementation of dynamic select to list all available API categories.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,6 @@ import Home from './pages/Home'
 
 function App() {
 
-  let apiList = [];
-  let categoriesList = [];
-
-  async function init() {
-    try {
-      // Fetching data from the main API
-      const response = await fetch("https://api.publicapis.org/entries");
-      const responseJson = await response.json();
-      // Creating the list of all APIs
-      apiList = [...responseJson.entries];
-      // Creating the list of all categories
-      for (let i in apiList) {
-        if (!categoriesList.includes(apiList[i].Category)) {
-          categoriesList = [...categoriesList, apiList[i].Category].sort();
-        }
-      }
-    }
-    catch(error) {
-      console.log("Oh no! Fetch error: ", error);
-      alert("Oups ! Un problème est survenu ! ");
-    }
-  }
-  init();
-
   return (
     <Home />
   );
--- src/components/Home/Main/index.jsx
+++ src/components/Home/Main/index.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Main() {
+
+  const [apiList, setApiList] = useState([]);
+  const [categoriesList, setCategoriesList] = useState([]);
+
+  useEffect( () => {
+    async function fetchData() {
+      try {
+        // Fetching data from the main API
+        const response = await fetch("https://api.publicapis.org/entries");
+        const responseJson = await response.json();
+        // Creating the list of all APIs
+        setApiList([...responseJson.entries]);
+      }
+      catch(error) {
+        console.log("Oh no! Fetch error: ", error);
+        alert("Oups ! Un problème est survenu ! ");
+      }
+    }
+    fetchData()
+  }, []);
+
+  useEffect( () => {
+    async function createCategoriesList() {
+      // Creating the list of all categories
+      for (let i in apiList) {
+        if (!categoriesList.includes(apiList[i].Category)) {
+          setCategoriesList([...categoriesList, apiList[i].Category].sort());
+        }
+      }
+    }
+    createCategoriesList()
+  }, [apiList, categoriesList]);
+
   return (
     <main>
-
-
+      <select name="category">
+        {categoriesList.map((el, idx) => (
+          <option key={`category-${idx}`} >{el}</option>
+        ))}
+      </select>
     </main>
   )
 }
